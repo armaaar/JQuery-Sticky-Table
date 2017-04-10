ab9f686a00cae84d6e1f48c736276277d24b41a5
Ahmed Rafik <ahmedrafik_maaar@outlook.com>
2017-04-10 15:46:42 +0200

    - Made sure internal table doesn't have padding - Fixed border issue in sticky cells

--- index.php
+++ index.php
@@ -44,6 +44,7 @@
             overflow: auto;
             border-top: 1px solid #ddd;
             border-bottom: 1px solid #ddd;
+            padding: 0 !important;
         }
         .table-sticky table {
             margin-bottom: 0;
@@ -54,12 +55,13 @@
             background-color: #fff;
             border-top: 0;
             position: relative;
+            outline: 1px solid #ddd;
             z-index: 5;
         }
         .table-sticky table td.sticky-cell, .table-sticky table th.sticky-cell
         {
             background-color: #fff;
-            border-right: 1px solid #ddd;
+            outline: 1px solid #ddd;
             position: relative;
             z-index: 10;
         }
@@ -76,25 +78,25 @@
 
      <script type="text/javascript">
         jQuery(function($){
-            $(".sticky-headers").scroll(function(){
-                $(this).find("table tr.sticky-row th").css('top', $(this).scrollTop());
-                $(this).find("table tr.sticky-row td").css('top', $(this).scrollTop());
-            });
-            $(".sticky-ltr-cells").scroll(function(){
-                $(this).find("table th.sticky-cell").css('left', $(this).scrollLeft());
-                $(this).find("table td.sticky-cell").css('left', $(this).scrollLeft());
-            });
-            $(".sticky-rtl-cells").scroll(function(){
-                var maxScroll = $(this).find("table").width() - $(this).width();
-                $(this).find("table th.sticky-cell").css('right', maxScroll - $(this).scrollLeft());
-                $(this).find("table td.sticky-cell").css('right', maxScroll - $(this).scrollLeft());
-            });
+                $(".sticky-headers").scroll(function(){
+                    $(this).find("table tr.sticky-row th").css('top', $(this).scrollTop());
+                    $(this).find("table tr.sticky-row td").css('top', $(this).scrollTop());
+                });
+                $(".sticky-ltr-cells").scroll(function(){
+                    $(this).find("table th.sticky-cell").css('left', $(this).scrollLeft());
+                    $(this).find("table td.sticky-cell").css('left', $(this).scrollLeft());
+                });
+                $(".sticky-rtl-cells").scroll(function(){
+                    var maxScroll = $(this).find("table").width() - $(this).width();
+                    $(this).find("table th.sticky-cell").css('right', maxScroll - $(this).scrollLeft());
+                    $(this).find("table td.sticky-cell").css('right', maxScroll - $(this).scrollLeft());
+                });
 
         });
      </script>
-    <body dir="rtl">
+    <body dir="ltr">
         <h1>Random things</h1>
-        <div class="table-sticky sticky-headers sticky-rtl-cells">
+        <div class="table-sticky sticky-headers sticky-ltr-cells">
             <table class="table table-striped">
                 <thead>
                     <tr class="sticky-row">
