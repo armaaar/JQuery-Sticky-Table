0cc07109786e5ae2f7eb9eb567016940bccf2e9c
Ahmed Rafik <ahmedrafik_maaar@outlook.com>
2018-01-10 17:00:00 +0200

    Fix: Disable smooth scrolling on sticky-table for mouse wheel event on IE11

--- jquery.stickytable.js
+++ jquery.stickytable.js
@@ -1,4 +1,13 @@
 jQuery(document).on('stickyTable', function() {
+    if(navigator.userAgent.match(/Trident\/7\./)) {
+      $('.sticky-table').on("mousewheel", function (event) {
+        console.log(event);
+          event.preventDefault();
+          var wd = event.originalEvent.wheelDelta;
+          var csp = $(this).scrollTop();
+          $(this).scrollTop(csp - wd);
+      });
+    }
     $(".sticky-headers").scroll(function() {
         $(this).find("table tr.sticky-row th").css('top', $(this).scrollTop());
         $(this).find("table tr.sticky-row td").css('top', $(this).scrollTop());
