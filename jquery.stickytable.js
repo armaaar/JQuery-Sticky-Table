c5b68af620c01f569eb66c675f0032063d66f03f
Ahmed Rafik <ahmedrafik_maaar@outlook.com>
2018-02-02 21:23:06 +0200

    Fix: Call scroll events upon triggering stickyTable Event

--- jquery.stickytable.js
+++ jquery.stickytable.js
@@ -11,16 +11,16 @@ jQuery(document).on('stickyTable', function() {
     $(".sticky-headers").scroll(function() {
         $(this).find("table tr.sticky-row th").css('top', $(this).scrollTop());
         $(this).find("table tr.sticky-row td").css('top', $(this).scrollTop());
-    });
+    }).scroll();
     $(".sticky-ltr-cells").scroll(function() {
         $(this).find("table th.sticky-cell").css('left', $(this).scrollLeft());
         $(this).find("table td.sticky-cell").css('left', $(this).scrollLeft());
-    });
+    }).scroll();
     $(".sticky-rtl-cells").scroll(function() {
         var maxScroll = $(this).find("table").prop("clientWidth") - $(this).prop("clientWidth");
         $(this).find("table th.sticky-cell").css('right', maxScroll - $(this).scrollLeft());
         $(this).find("table td.sticky-cell").css('right', maxScroll - $(this).scrollLeft());
-    });
+    }).scroll();
 });
 $( document ).ready(function(){
     $( document ).trigger( "stickyTable" );
